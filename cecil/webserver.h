970919317c917ece115a5b5a1726dec8e995fda1
dargles <d.argles@gmx.com>
2023-07-07 00:26:40 +0100

    Update webServer.h

--- cecil/webserver.h
+++ cecil/webserver.h
@@ -1,99 +1,94 @@
 /**
- * Class definition for webserver
+ * Routines to handle web access
  * 
- * The webserver class handles anything to do with our webserver
+ * !NOTE! - this class requires serial output to have been started up first.
+ * 
  * @author  David Argles, d.argles@gmx.com
- * @version 21Feb2019, updated for ESP32 18Aug2021
+ * @version 06July2023 12:30h
+ * @param   WiFiClient  client  The incoming WiFi client
  */
 
-#include <WiFi.h>       // Include the ESP32 WiFi header file
-#include <WiFiClient.h>
-#include <WiFiAP.h>
 #include "compiler.h"
 
-#define LED_BUILTIN 2
+compiler CecilProg;
 
-const char *ssid = "Cecil";
-const char *password = "Cecil123";
-
-WiFiServer server(80);  // Initialize the ESP webserver on Port 80
-compiler   myCompiler;
-
-class webserver
+ void sendHead(WiFiClient client)
 {
-  private:
+  // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+  // and a content-type so the client knows what's coming, then a blank line:
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-type:text/html");
+  client.println();
+  // Now send the initial HTML
+  client.println("<!DOCTYPE html>");
+  client.println("<html lang=\"en\">");
+  client.println("  <head>");
+  client.println("    <meta charset=\"UTF-8\">");
+  client.println("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+  String line = "    <title>";
+  line = line + PROG + "</title>";
+  client.println(line);
+  client.println("  </head>");
+  client.println("  <body>");
+  client.println("    <h1>CECIL</h1>");
+  client.println("    <section>");
+  client.println("      <h2>Program</h2>");
+  client.println("      <pre>");
+  client.println(CecilProg.program);
+  client.println("      </pre>");
+  client.println("    </section>");
 
-  void  buildPageTop(){
-    pageTop += "<!DOCTYPE html>\n";
-    pageTop += "<html>\n";
-    pageTop += "  <head>\n";
-    pageTop += "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n";
-    pageTop += "  </head>\n";
-    pageTop += "  <body>\n";
-    pageTop += "    <h3>CECIL</h3>\n"; 
-    pageTop += "    <form action='form.php' method='get'>\n";
-    return;
-  }
+  return;
+}
 
-  void  buildPageBottom(){
-    pageBottom += "      <input type='submit' value='Compile'>\n";
-    pageBottom += "      <input type='submit' value='Run'>\n";
-    pageBottom += "    </form>\n"; 
-    pageBottom += "  </body>\n";
-    pageBottom += "</html>\n";
-    return;
-  }
+void sendTail(WiFiClient client)
+{
+  // Send the final HTML
+  client.println("  </body>");
+  client.println("</html>");
+  return;
+}
 
-  public:
-  
-  bool live = false;
-  
-  //String ipAddress = "Server IP is: ";  // Set up our ipAddress message
+void serviceWebRequest(WiFiClient client)
+{
+    Serial.println("New Client.");           // print a message out the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        if (c == '\n') {                    // if the byte is a newline character
 
-  String pageTop = "";
-  String pageContent = "";
-  String pageBottom = "";
-  
-  // The constructor
-  webserver(){
-  }
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // Send the headers
+            sendHead(client);
+            // the content of the HTTP response follows the header:
+            client.print("Click <a href=\"/H\">here</a> to turn the LED on pin 5 on.<br>");
+            client.print("Click <a href=\"/L\">here</a> to turn the LED on pin 5 off.<br>");
+            sendTail(client);
+            // The HTTP response ends with another blank line:
+            client.println();
+            // break out of the while loop:
+            break;
+          } else {    // if you got a newline, then clear currentLine:
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
 
-  void begin(){
-    Serial.println("Starting up web server:");
-    WiFi.softAP(ssid, password);     // Provide the (SSID, password); . 
-    IPAddress myIP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(myIP);
-    server.begin();
-    Serial.println("Server started");
-    //IPAddress HTTPS_ServerIP = WiFi.softAPIP(); // Obtain the IP of the Server 
-    //ipAddress += HTTPS_ServerIP;
-    live = true;
-    buildPageTop();
-    buildPageBottom();
-    return;
-  }
-
-  void pageAdd(String content){
-    pageContent += content;
-  }
-
-  void servePage(String buffContents){
-    WiFiClient client = server.available();
-    if(client) 
-    {
-      pageContent += "      <textarea id=\"program\" width = \"70\" height = \"80\">";
-      pageContent += myCompiler.program;
-      pageContent += "</textarea>\n";
-      myCompiler.compile();
-      pageContent += myCompiler.output;
-      if(myCompiler.compiled)pageContent += "Program compiled\n";
-
-      client.flush();             // Clear previous info in the stream 
-      client.print(pageTop);      // Send the page top to the client 
-      client.print(pageContent);  // Send the page content to the client 
-      client.print(pageBottom);   // Send the page bottom to the client 
+        // Check to see if the client request was "GET /H" or "GET /L":
+        if (currentLine.endsWith("GET /H")) {
+          digitalWrite(BUILTIN_LED, HIGH);               // GET /H turns the LED on
+        }
+        if (currentLine.endsWith("GET /L")) {
+          digitalWrite(BUILTIN_LED, LOW);                // GET /L turns the LED off
+        }
+      }
     }
+    // close the connection:
     client.stop();
-  }
-};
+    Serial.println("Client Disconnected.");
+}
